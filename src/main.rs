0020140d9e5ab549473fb23867ba50f35abfbf8b
Max Nachlinger <mnachlinger@walmartlabs.com>
2020-10-25 13:16:52 -0700

    Add actix-web REST API

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,59 @@
-use sqlx::mssql::{MssqlPool, MssqlConnectOptions};
+#[macro_use]
+extern crate log;
 
-#[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
-    let options = MssqlConnectOptions::new()
-        .host("127.0.0.1")
-        .port(1433)
-        .database("test")
-        .username("test")
-        .password("test_p4ssword");
+use dotenv::dotenv;
+use listenfd::ListenFd;
+use std::env;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use sqlx::mssql::MssqlPool;
+use anyhow::Result;
 
-    let pool = MssqlPool::connect_with(options).await?;
+// import todo module (routes and model)
+mod todo;
 
-    let row: (i32,String) = sqlx::query_as("SELECT id, name FROM test_table WHERE id = 1")
-        .fetch_one(&pool).await?;
+// default / handler
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body(r#"
+        Welcome to Actix-web with SQLx Todos example.
+        Available routes:
+        GET /todos -> list of all todos
+        POST /todo -> create new todo, example: { "description": "learn actix and sqlx", "done": false }
+        GET /todo/{id} -> show one todo with requested id
+        PUT /todo/{id} -> update todo with requested id, example: { "description": "learn actix and sqlx", "done": true }
+        DELETE /todo/{id} -> delete todo with requested id
+    "#
+    )
+}
 
-    dbg!(row);
+#[actix_web::main]
+async fn main() -> Result<()> {
+    dotenv().ok();
+    env_logger::init();
+
+    // this will enable us to keep application running during recompile: systemfd --no-pid -s http::5000 -- cargo watch -x run
+    let mut listenfd = ListenFd::from_env();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let db_pool = MssqlPool::connect(&database_url).await?;
+
+    let mut server = HttpServer::new(move || {
+        App::new()
+            .data(db_pool.clone()) // pass database pool to application so we can access it inside handlers
+            .route("/", web::get().to(index))
+            .configure(todo::init) // init todo routes
+    });
+
+    server = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => server.listen(listener)?,
+        None => {
+            let host = "127.0.0.1";
+            let port = 8080;
+            server.bind(format!("{}:{}", host, port))?
+        }
+    };
+
+    info!("Starting server");
+    server.run().await?;
 
     Ok(())
 }
